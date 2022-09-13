574eb715e4e86d2394bdad5103f62d258db54e3f
Alexandre Henrique da Rocha Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2022-09-13 00:28:24 -0300

    primeras respostas da api

--- src/pages/negocios/index.tsx
+++ src/pages/negocios/index.tsx
@@ -1,6 +1,33 @@
+import { Button } from '@chakra-ui/react';
+import axios from 'axios';
+
 function Negocios() {
-  
-  return <h1>Negócios1</h1>;
+  const consulta = async () => {
+    const id = 15870204711
+    const url2 = `/api/request/get/contatos/${id}`;
+
+    await axios({
+      method: 'GET',
+      url: url2,
+      headers: {
+        'Content-Type': 'application/json'
+      },
+    })
+      .then((response) => {
+        console.log(response.data)
+        const json = response.data
+        const result = json.filter((iten: any) => iten.id === id)
+        console.log(result[0])
+      })
+      .then((json) => console.log(json))
+      .catch((err) => console.log(err));
+  };
+
+  return (
+    <>
+      <Button onClick={consulta}>consulta</Button>
+    </>
+  );
 }
 
 export default Negocios;
