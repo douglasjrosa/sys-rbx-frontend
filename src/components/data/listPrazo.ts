e0bcc7695e2406a2c575b01e7a53a004a1576d42
Alexandre Henrique R Araujo <64115785+alexandre-henrique-rp@users.noreply.github.com>
2023-03-28 17:13:51 -0300

    modulo data prazo

---
+++ src/components/data/listPrazo.ts
@@ -0,0 +1,70 @@
+export const listPrazo = [
+  {
+    id: 2,
+    titulo: '15 Dias',
+  },
+  {
+    id: 3,
+    titulo: '28 Dias',
+  },
+  {
+    id: 4,
+    titulo: '30 Dias',
+  },
+  {
+    id: 5,
+    titulo: '45 Dias',
+  },
+  {
+    id: 6,
+    titulo: '90 Dias',
+  },
+  {
+    id: 7,
+    titulo: '10 / 20 Dias',
+  },
+  {
+    id: 8,
+    titulo: '28 / 35 Dias',
+  },
+  {
+    id: 9,
+    titulo: '28 / 35 / 42 Dias',
+  },
+  {
+    id: 10,
+    titulo: '30 / 40 / 50 Dias',
+  },
+  {
+    id: 11,
+    titulo: '30 / 60 / 90 Dias',
+  },
+  {
+    id: 12,
+    titulo: 'Latam',
+  },
+  {
+    id: 13,
+    titulo: 'Vanderlei',
+  },
+  {
+    id: 14,
+    titulo: 'Dentemed',
+  },
+  {
+    id: 15,
+    titulo: 'Pocelli',
+  },
+  {
+    id: 16,
+    titulo: 'Alliage',
+  },
+  {
+    id: 17,
+    titulo: '7 Dias',
+  },
+  {
+    id: 18,
+    titulo: '15 Dias',
+  },
+];
