8af01f9326343542a5e2f4a524d35293a5af6c80
douglasjrosa <63507739+douglasjrosa@users.noreply.github.com>
2024-10-05 22:50:23 -0300

    console test

--- src/pages/api/testRbx.ts
+++ src/pages/api/testRbx.ts
@@ -4,7 +4,7 @@ import { PostLoteRibermmax } from './db/nLote/lib/postLoteRibermax'
 export default async function handler ( req: NextApiRequest, res: NextApiResponse ) {
 	try {
 		const resp = await PostLoteRibermmax( "2106" )
-
+		console.log({resp})
 		res.status( 201 ).json( resp )
 	} catch ( error ) {
 		res.status( 500 ).json( { message: 'Error al obtener el lote' } )
